d9647f66d682d11a33872d7670f5aaa7115b42ae
Irina Lopatina <Irina Lopatina>
2017-11-06 22:44:41 +0100

    Add project files.

---
+++ Exam70483_1/Exam70483_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam70483_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //""
+            //
+            //Irina 
+            string dd = "j";
+        }
+    }
+}
