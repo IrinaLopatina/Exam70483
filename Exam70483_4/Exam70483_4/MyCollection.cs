d172e8e3a6fb3bbf6a05d5107ccc4dfec4bdde77
irinalopatina <irinalopatina@hotmail.com>
2017-11-13 15:46:51 +0100

    IEnumerable<T>

---
+++ Exam70483_4/Exam70483_4/MyCollection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam70483_4
+{
+    class MyCollection<T> : IEnumerable<T> //,IList<T>, ICollection<T> 
+    {
+        //T[] data = new T[10]; //starts from []
+
+        List<T> data = new List<T>();
+
+        public void Add(T item)
+        {
+            data.Add(item);
+        }
+
+        #region IEnumerable<T>
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var t in data)
+            {
+                yield return t;
+            }
+            //return data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() // Explicit implementation (keeps it hidden)
+        {
+            return GetEnumerator();
+        }
+        #endregion IEnumerable<T>
+    }
+}
--- Exam70483_4/Exam70483_4/Program.cs
+++ Exam70483_4/Exam70483_4/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace Exam70483_4
 {
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            MyCollection<Person> myCollection = new MyCollection<Person>
+            {
+                new Person { Name = "Karsten", Age = 46, Address = "Norway" },
+                new Person { Name = "Nina", Age = 67, Address = "Russia" },
+                new Person { Name = "Valeria", Age = 11, Address = "Norway" },
+                new Person { Name = "Nikita", Age = 16, Address = "Russia" },
+                new Person { Name = "Marina", Age = 4, Address = "Norway" },
+                new Person { Name = "Irina", Age = 43, Address = "Norway" },
+                new Person { Name = "Nikolai", Age = 68, Address = "Russia" },
+                new Person { Name = "Viktor", Age = 7, Address = "Norway" }
+            };
+
+            Console.WriteLine("Total person: {0} \n\n", myCollection.Count());
+            foreach (var person in myCollection)
+            {
+                Console.WriteLine("Name: {0}, Age: {1}, Address: {2}", person.Name, person.Age, person.Address);
+            }
         }
+
     }
+
+    /* Yield return example 
+       static void Main(string[] args)
+       {
+           foreach (string s in Foo())
+           {
+               Console.WriteLine("Printing.... {0}", s);
+           }
+       }
+
+       static IEnumerable<string> Foo()
+       {
+           yield return "1 string";
+           yield return "2 string";
+           yield return "3 string";
+           yield return "4 string";
+       }
+       */
 }
