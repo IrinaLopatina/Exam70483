848158c25ddc4788e4027db92d2e9505e025ccc0
irinalopatina <irinalopatina@hotmail.com>
2017-12-12 14:04:49 +0100

    Exam70483_7, first version

---
+++ Exam70483_7/Exam70483_7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exam70483_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebRequester req = new WebRequester())
+            {
+            }
+            Console.WriteLine("The end");
+        }
+    }
+}
---
+++ Exam70483_7/Exam70483_7/WebRequester.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.IO;
+
+namespace Exam70483_7
+{
+    class WebRequester : IDisposable
+    {
+        const string uri = "http://www.google.com";
+
+        //Unmanaged resources
+        HttpWebResponse response;
+        Stream dataStream;
+        StreamReader reader;
+
+        public WebRequester()
+        {
+            // Create a request for the URL. 		
+            WebRequest request = WebRequest.Create(uri);
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            // Get the response.
+            response = (HttpWebResponse)request.GetResponse();
+            // Display the status.
+            Console.WriteLine("Response status description: {0}", response.StatusDescription);
+
+            // Get the stream containing content returned by the server.
+            dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            Console.WriteLine(responseFromServer);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~WebRequester()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // Cleanup the streams and the response.
+                reader.Dispose();
+                dataStream.Dispose();
+                response.Dispose();
+            }
+        }
+    }
+}
