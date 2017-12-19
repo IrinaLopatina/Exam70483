6ec7ca5986ef1f9ace1fadfd5f01d6550d6e401b
irinalopatina <irinalopatina@hotmail.com>
2017-12-19 23:21:52 +0100

    70483_8

---
+++ Exam70483_8/Exam70483_8/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exam70483_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await DoTimeConsumingTaskAsync();
+        }
+
+        private Task DoTimeConsumingTaskAsync()
+        {
+            Task task = Task.Run(() =>
+            {
+                using (var htmlReader = new HTML_Reader())
+                {
+
+                    var html = htmlReader.GetHtml();
+                    //label1.Text = textBox1.Text;//html;
+                    //label1.Text = html.Substring(0, 20);
+                    textBox2.Text = html;//.Substring(0, 200);
+                }
+            });
+
+            return task;
+        }
+    }
+}
---
+++ Exam70483_8/Exam70483_8/HTML_Reader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace Exam70483_8
+{
+    class HTML_Reader : IDisposable
+    {
+        //Resourses to dispose
+        private HttpWebResponse webResponse;
+        private Stream dataStream;
+        private StreamReader streamReader;
+
+
+        public string GetHtml(string uri = "http://www.google.com")
+        {
+            WebRequest webRequest = WebRequest.Create(uri);
+            webRequest.Credentials = CredentialCache.DefaultCredentials;
+
+            webResponse = (HttpWebResponse)webRequest.GetResponse();
+
+            dataStream = webResponse.GetResponseStream();
+
+            streamReader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = streamReader.ReadToEnd();
+
+
+            return responseFromServer;
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                streamReader?.Dispose();
+                dataStream?.Dispose();
+                webResponse?.Dispose();
+            }
+        }
+
+        ~HTML_Reader()
+        {
+            Dispose(false);
+        }
+    }
+}
