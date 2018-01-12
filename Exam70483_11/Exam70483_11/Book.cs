33df8ad40da1a3dce46487122d748ec68d86ac9c
irinalopatina <irinalopatina@hotmail.com>
2018-01-12 16:19:00 +0100

    tt

---
+++ Exam70483_11/Exam70483_11/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam70483_11
+{
+    class Book
+    {
+        public string book; //ID!!!!
+        public string author;
+        public string title;
+        public string genre;
+        public double price;
+        public DateTime publish_date;
+        public string description;
+    }
+}
---
+++ Exam70483_11/Exam70483_11/Catalog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam70483_11
+{
+    class Catalog
+    {
+        public List<Book> bookList;
+    }
+}
