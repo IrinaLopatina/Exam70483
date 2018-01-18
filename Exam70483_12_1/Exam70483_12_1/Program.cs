cd7c43dfbddcae2c7b38821ce4e48a64052afe2e
irinalopatina <irinalopatina@hotmail.com>
2018-01-18 13:41:38 +0100

    12_1

---
+++ Exam70483_12_1/Exam70483_12_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam70483_12_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SchoolDB db = new SchoolDB();
+            var students = db.Student.ToList();
+
+            students.ForEach(s => Console.WriteLine("Id is: {0}, Name is: {1}", s.StudentId, s.StudentName));
+
+            Student st = new Student
+            {
+                StudentId = 5,
+                StudentName = "Karsten Vileid"
+            };
+
+            db.Student.Add(st);
+            db.SaveChanges();
+            Console.WriteLine("Student Added");
+        }
+    }
+}
---
+++ Exam70483_12_1/Exam70483_12_1/SampleEFmodel.Designer.cs
@@ -0,0 +1,10 @@
+﻿// T4 code generation is enabled for model 'C:\Users\kvileid\source\repos\Exam70483\Exam70483_12_1\Exam70483_12_1\SampleEFmodel.edmx'. 
+// To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
+// property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
+// is open in the designer.
+
+// If no context and entity classes have been generated, it may be because you created an empty model but
+// have not yet chosen which version of Entity Framework to use. To generate a context class and entity
+// classes for your model, open the model in the designer, right-click on the designer surface, and
+// select 'Update Model from Database...', 'Generate Database from Model...', or 'Add Code Generation
+// Item...'.
