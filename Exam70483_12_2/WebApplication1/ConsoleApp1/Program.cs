fb34b557eb80fe36b7f0322a69be709a1d910d31
irinalopatina <irinalopatina@hotmail.com>
2018-01-19 23:51:44 +0100

    WebService

---
+++ Exam70483_12_2/WebApplication1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyService.WebService1SoapClient proxy = new MyService.WebService1SoapClient();
+
+            int addResult = proxy.Add(5, 19);
+            int subtractResult = proxy.Subtract(22, 12);
+
+            Console.WriteLine("Addition result is: {0}", addResult);
+            Console.WriteLine("Subtraction result is: {0}", subtractResult);
+        }
+    }
+}
